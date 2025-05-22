7cbeb9ca254761b4e0d8839fa8de6c7409547b40
Valentin Arkov <47300530+Valentin-Arkov@users.noreply.github.com>
2025-05-23 01:06:58 +0500

    Add files via upload

---
+++ CS/find_max_csv.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static int FindMax(int[] nums)
+    {
+        int max = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+            if (max < nums[i])
+                max = nums[i];
+        return max;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("n;T(n)");
+        for (int n = 100_000_000; n <= 300_000_000; n += 50_000_000)
+        {
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+                nums[i] = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                int x_max = FindMax(nums);
+                stopwatch.Stop();
+                Console.WriteLine($"{n};{stopwatch.Elapsed.TotalSeconds:F10}");
+            }
+        }
+    }
+}
