42d698414a375add922b48cd9382341f2836b6e9
Valentin Arkov <47300530+Valentin-Arkov@users.noreply.github.com>
2025-06-19 23:09:43 +0500

    Add files via upload

---
+++ CS/bubble.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program {
+    static void BubbleSort(int[] arr) {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+    }
+    static void Main() {
+        int[] x = { 3, 2, 1, 4, 5 };
+        Console.WriteLine(string.Join(" ", x));
+        BubbleSort(x);
+        Console.WriteLine(string.Join(" ", x));
+    }
+}
+
+// (c) Valentin Arkov
